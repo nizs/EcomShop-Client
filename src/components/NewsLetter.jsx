1acc6c07ceb5b2fdcf5081bd80b294a7815d7960
Tauhid Afser <tauhidafser03@gmail.com>
2025-04-13 18:04:43 +0600

    collection page left filter done

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -10,7 +10,7 @@ const NewsLetter = () => {
             <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, molestiae!</p>
             <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
                 <input onSubmit={onSubmitHandler} className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter Your Email' required />
-                <button className='bg-black text-white text-xs px-10 py-4' type='submit'>SUBSCRIBE</button>
+                <button className='bg-black text-white text-xs px-10 py-4 cursor-pointer' type='submit'>SUBSCRIBE</button>
             </form>
         </div>
     );
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { assets } from '../assets/assets';
+import { ShopContext } from '../context/ShopContext';
 
 const Collection = () => {
+    const { products } = useContext(ShopContext);
+    const [showFilter, setShowFilter] = useState(false);
     return (
-        <div>
-            <h1>This will be our collection page</h1>
+        <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+            {/* Filter */}
+            <div className='min-w-60'>
+                <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+                    <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+                </p>
+                {/* Filter Category */}
+                <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+                    <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+                    <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Men'} />Men
+                        </p>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Women'} />Women
+                        </p>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Kids'} />Kids
+                        </p>
+                    </div>
+                </div>
+                {/* Filter-Sub-Category */}
+                <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
+                    <p className='mb-3 text-sm font-medium'>SUB CATEGORIES</p>
+                    <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Tops'} />Tops
+                        </p>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Bottoms'} />Bottoms
+                        </p>
+                        <p className='flex gap-2'>
+                            <input className='w-3' type="checkbox" value={'Accessories'} />Accessories
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
